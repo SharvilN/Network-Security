793e38c7863c2809e5915540d400342740236e21
Sharvil Nagarkar <sharvilnagarkar@gmail.com>
2018-10-20 14:51:36 +0530

    3DES

---
+++ TripleDES/src/tripleDES/DESFunction.java
@@ -0,0 +1,134 @@
+package tripleDES;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DESFunction {
+	
+	private static final ArrayList<Integer> EXPANSION_PBOX_TABLE = new ArrayList<Integer> (Arrays.asList(32, 1, 2, 3, 4, 5,
+		4, 5, 6, 7, 8, 9,
+		8, 9, 10, 11, 12, 13,
+		12, 13, 14, 15, 16, 17,
+		16, 17, 18, 19, 20, 21,
+		20, 21, 22, 23, 24, 25,
+		24, 25, 26, 27, 28, 29,
+		28, 29, 30, 31, 32, 1));
+	
+	private static final ArrayList<Integer> STRAIGHT_PERMUTATION_TABLE = new ArrayList<Integer> (Arrays.asList(16 ,  7 , 20  ,21,
+        29 , 12 , 28 , 17,
+        1 , 15,  23,  26,
+        5 , 18 , 31  ,10,
+        2  , 8 , 24 , 14,
+        32  ,27,   3 ,  9,
+        19  ,13,  30,   6,
+        22 , 11  , 4 , 25));
+
+	private static final int[][][] SBOX_PERMUTAION_TABLE = new int[][][]{
+	    {
+	        {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
+	        {0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
+	        {4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
+	        {15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13},
+	    },
+	    {
+	        {15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10},
+	        {3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5},
+	        {0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15},
+	        {13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9},
+	    },
+
+
+	    {
+	        {10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8},
+	        {13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1},
+	        {13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7},
+	        {1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12},
+	    },
+	    {
+	        {7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15},
+	        {13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9},
+	        {10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4},
+	        {3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14},
+	    },
+	    {
+	        {2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9},
+	        {14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6},
+	        {4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14},
+	        {11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3},
+	    },
+	    {
+	        {12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11},
+	        {10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8},
+	        {9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6},
+	        {4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13},
+	    },
+	    {
+	        {4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1},
+	        {13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6},
+	        {1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2},
+	        {6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12},
+	    },
+	    {
+	        {13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7},
+	        {1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2},
+	        {7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8},
+	        {2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11},
+	    }};
+	
+	public String coreFunction(String plainBlock32Bit, String keyString){
+		final String expandedString = pBoxExpansion(plainBlock32Bit);
+		final String xoredString = this.doXOR(expandedString, keyString);
+		final String sBoxPermutedString = this.sBoxPermutation(xoredString);
+		final String resutltantString = this.straightPermutation(sBoxPermutedString);
+		return resutltantString;
+	}
+
+	private String straightPermutation(final String sBoxPermutedString) {
+		String resultantString = "";
+		System.out.println("S box length : " + sBoxPermutedString.length());
+		for(int i=0; i<sBoxPermutedString.length(); i++){
+			resultantString += sBoxPermutedString.charAt(STRAIGHT_PERMUTATION_TABLE.get(i) - 1);
+		}
+		return resultantString;
+	}
+
+	private String doXOR(final String plainText32Bit, final String keyString) {
+		String xoredString = "";
+		for(int i=0; i<keyString.length(); i++){
+			if(plainText32Bit.charAt(i) != keyString.charAt(i)){
+				xoredString += "1";
+			}
+			else{
+				xoredString += "0";
+			}
+		}
+		return xoredString;
+	}
+
+	private String pBoxExpansion(final String plainText32Bit) {
+		String expandedString = "";
+		for(int i=0; i<EXPANSION_PBOX_TABLE.size(); i++){
+			expandedString += plainText32Bit.charAt(EXPANSION_PBOX_TABLE.get(i) - 1);
+		}
+		return expandedString;
+	}
+	
+	private String sBoxPermutation(final String xoredString){
+		String sBoxCompressedString = "";
+		for(int box=0; box<8; box++){
+			Integer rowInSBox = 2*Integer.parseInt(Character.toString(xoredString.charAt(6*box))) + Integer.parseInt(Character.toString(xoredString.charAt(6*box+5)));
+			Integer colInSBox = 8*Integer.parseInt(Character.toString(xoredString.charAt(6*box+1))) +
+								4*Integer.parseInt(Character.toString(xoredString.charAt(6*box+2))) +
+								2*Integer.parseInt(Character.toString(xoredString.charAt(6*box+3))) +
+								Integer.parseInt(Character.toString(xoredString.charAt(6*box+4)));
+//			System.out.println("Row and col in S Box : " + rowInSBox + " " + colInSBox);
+			String temp = Integer.toBinaryString(SBOX_PERMUTAION_TABLE[box][rowInSBox][colInSBox]);
+			while(temp.length()<4){
+				temp = "0" + temp;
+			}
+			sBoxCompressedString += temp;
+			
+		}
+		return sBoxCompressedString;
+	}
+}
